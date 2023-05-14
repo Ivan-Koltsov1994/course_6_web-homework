88db1cbf65430eac92090954f608138753de0d04
Ivan <ivan.koltsov.1994@mail.com>
2023-05-14 16:33:45 +0500

    add migration

--- catalog/models.py
+++ catalog/models.py
@@ -27,6 +27,8 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
 
+    created_at = models.DateField(verbose_name='Создан в', **NULLABLE)
+
     def __str__(self):
         return f'{self.name} {self.description}'
 
