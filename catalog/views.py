7aad62f9e6f597836db031e74a2b84209f4cf9e2
Ivan <ivan.koltsov.1994@mail.com>
2023-05-14 21:29:44 +0500

    display 5 latest products

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from catalog.models import Product,  Category
 
 def home(request):
+
+    # В контроллер отображения главной страницы добавить выборку последних 5 товаров и вывод их в консоль
+    latest_products = Product.objects.order_by('-creation_date')[:5]
+    for products in latest_products:
+        print(products.name)
+
     return render(request, 'catalog/home.html')
 
 
