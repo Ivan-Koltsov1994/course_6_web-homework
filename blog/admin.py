1241f911c8bce2beae8f959d6e19889311981449
Ivan <ivan.koltsov.1994@mail.com>
2023-06-04 19:11:50 +0500

    correct admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Record
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title','slug', 'content')
+    list_filter = ('title',)
+    search_fields = ('title',)
+
