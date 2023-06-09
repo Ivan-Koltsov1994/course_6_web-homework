0a84c30b33a0977df3158a938255b270071a4e5d
Ivan <ivan.koltsov.1994@mail.com>
2023-06-09 23:37:51 +0500

    add clean_name,clean_description in ProductForms forms.py

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from catalog.models import Product
 
+#Cписок запрещенных слов
+stop_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
 class ProductForms(forms.ModelForm):
     class Meta:
@@ -10,3 +12,16 @@ class ProductForms(forms.ModelForm):
         # fields = ('name', 'description')# Выбираем поля для формы
         # exclude = ('is_active', )  # исключает поле из формы
 
+    def clean_name(self):
+        """Функция проверяет исключения загрузки запрещенных продуктов в название"""
+        cleaned_data = self.cleaned_data['name']
+        if cleaned_data in stop_words:
+            raise forms.ValidationError('Запрещенное название')
+        return cleaned_data
+
+    def clean_description(self):
+        """Функция проверяет исключения загрузки запрещенных продуктов в описание"""
+        cleaned_data = self.cleaned_data['description']
+        if cleaned_data in stop_words:
+            raise forms.ValidationError('Запрещенное описание')
+        return cleaned_data
