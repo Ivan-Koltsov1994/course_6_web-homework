eb5c338f81e6cce06d51b7592c42c2001e8b676b
Ivan <ivan.koltsov.1994@mail.com>
2023-06-10 15:49:48 +0500

    add  Version formset

--- catalog/views.py
+++ catalog/views.py
@@ -54,6 +54,16 @@ class ProductUpdateView(UpdateView):
 
         return context_data
 
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object =form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+
+        return super().form_valid(form)
+
 class ProductDeleteView(DeleteView):
     """Класс для удаления Продуктов"""
     model = Product
