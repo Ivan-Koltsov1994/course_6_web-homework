1eb6de56b65d45a7bb7d3bf9d112a1f616a65d59
Ivan <ivan.koltsov.1994@mail.com>
2023-06-10 14:53:47 +0500

    add model Version

--- catalog/models.py
+++ catalog/models.py
@@ -44,3 +44,19 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ('name',)  # сортировка, '-name' - сортировка в обратном порядке
+
+
+class Version(models.Model):
+    """Класс модели Версии Продукта"""
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='продукт')
+    number_version = models.IntegerField(verbose_name='номер версии')
+    title_version = models.CharField(max_length=150, verbose_name='название версии')
+    is_active = models.BooleanField(verbose_name='признак текущей версии', default=True)
+
+    def __str__(self):
+        return f'{self.number_version} ({self.product})'
+
+    class Meta:
+        """Класс мета-настроек"""
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
